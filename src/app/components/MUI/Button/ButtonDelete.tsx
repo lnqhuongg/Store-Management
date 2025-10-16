2a534208bc6b8ff810c9a340b971309b787130ea
lnqhuongg <cuubong47@gmail.com>
2025-10-16 08:08:27 +0700

    cap nhat ngay 16/10/2025 - them modal cho nut them sua + bat su kien nut delete

--- src/app/components/MUI/Button/ButtonDelete.tsx
+++ src/app/components/MUI/Button/ButtonDelete.tsx
@@ -2,10 +2,17 @@ import Image from "next/image";
 import styleBtn from "../styleMaterials.module.css"
 import deleteIcon from "../../../../../public/icons/bin.png";
 
+// update 16/10/2025
+interface DeleteButtonProps {
+  onClick: () => void;
+}
 
-export default function ButtonDelete() {
+export default function ButtonDelete( { onClick }: DeleteButtonProps ) {
     return (
-        <button className="btn btn-sm btn-danger">
+        <button type="button"
+                className="btn btn-sm btn-danger"
+                onClick={onClick}
+        >
             <Image
                 src={deleteIcon}
                 alt="Xóa"
--- src/app/components/MUI/Button/ButtonEdit.tsx
+++ src/app/components/MUI/Button/ButtonEdit.tsx
@@ -2,10 +2,17 @@ import Image from "next/image";
 import styleBtn from "../styleMaterials.module.css"
 import editIcon from "../../../../../public/icons/edit.png";
 
+// update 16/10/2025
+interface EditButtonProps {
+  onClick: () => void;
+}
 
-export default function ButtonEdit() {
+export default function ButtonEdit( { onClick }: EditButtonProps ) {
     return (
-        <button className="btn btn-sm btn-warning me-2">
+        <button type="button"
+                className="btn btn-sm btn-warning me-2"
+                onClick={onClick}
+        >
             <Image
                 src={editIcon}
                 alt="Chỉnh sửa"
