a9bc2437d94c76431ef427592fb52ac7566b1c9a
Nguyễn Hùng Lê Trung <trungbekerteo001@gmail.com>
2025-11-10 22:24:45 +0700

    Load, add, edit, delete fe

--- src/app/routes/Routes.tsx
+++ src/app/routes/Routes.tsx
@@ -6,7 +6,7 @@ export interface RouteItem {
 export const adminRoutes: RouteItem[] = [
     { name: 'Quản lý Sản phẩm', path: '/admin/SanPham' },
     { name: 'Quản lý Loại sản phẩm', path: '/admin/LoaiSanPham' },
-    { name: 'Quản lý Nhà cung cấp', path: '/admin/NCC' },
+    { name: 'Quản lý Nhà cung cấp', path: '/admin/NhaCungCap' },
     { name: 'Quản lý Tài khoản nhân viên', path: '/admin/NhanVien' },
     { name: 'Quản lý Thông tin khách hàng', path: '/admin/KhachHang' },
     { name: 'Quản lý Mã giảm giá', path: '/admin/MGG' },
